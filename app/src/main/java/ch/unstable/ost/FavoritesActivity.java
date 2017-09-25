a7db502a7e06fed3b54cd0b38c58a8881adf0927
Coffeemakr <github@unstable.ch>
2017-09-25 13:53:47 +0200

    Improve error handling (#16) and remove fab

--- app/src/main/java/ch/unstable/ost/FavoritesActivity.java
+++ app/src/main/java/ch/unstable/ost/FavoritesActivity.java
@@ -1,33 +1,25 @@
 package ch.unstable.ost;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-
-import java.util.List;
 
 import ch.unstable.ost.database.Databases;
 import ch.unstable.ost.database.dao.FavoriteConnectionDao;
-import ch.unstable.ost.database.model.FavoriteConnection;
 import ch.unstable.ost.lists.favorite.FavoritesAdapter;
-import io.reactivex.FlowableSubscriber;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
-
-import static com.google.common.base.Preconditions.checkNotNull;
 
 public class FavoritesActivity extends AppCompatActivity {
 
     private FavoriteConnectionDao mFavoritesDao;
-    private FlowableSubscriber<? super List<FavoriteConnection>> favoritesConsumer;
     private FavoritesAdapter mFavoritesAdapter;
-    private Disposable mDisposable;
+    private CompositeDisposable mDisposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,30 +27,34 @@ public class FavoritesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_favorites);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        checkNotNull(getSupportActionBar(), "getSupportActionBar").setDisplayHomeAsUpEnabled(true);
+        //noinspection ConstantConditions
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         mFavoritesAdapter = new FavoritesAdapter();
         RecyclerView favorites = findViewById(R.id.favorites);
         favorites.setAdapter(mFavoritesAdapter);
         favorites.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
 
+        mDisposable = new CompositeDisposable();
         mFavoritesDao = Databases
                 .getCacheDatabase(this)
                 .favoriteConnectionDao();
+    }
 
-        mDisposable = mFavoritesDao.getFavoriteConnections()
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Disposable disposable = mFavoritesDao.getFavoriteConnections()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(mFavoritesAdapter);
+                .subscribe(mFavoritesAdapter, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        NavHelper.startErrorActivity(FavoritesActivity.this, throwable);
+                    }
+                });
+        mDisposable.add(disposable);
+
     }
 
     @Override
